7540edf523d9066ffe9ce38fd097bf17c79680a5
jane.gleadall <jane.gleadall@dwp.gsi.gov.uk>
2017-10-19 18:00:45 +0100

    Added Save handover function and handover-edit page, and made changes to json data and handover model

--- app/models/handover.js
+++ app/models/handover.js
@@ -1,25 +1,34 @@
 class Handover {
-    constructor(id, claimantNino, staffId, owningOfficeId, benefitId, typeId, reasonId, callback, priority) {
+    constructor(id, nino, staffId, owningOfficeId, benefitId, typeId, reasonId, callback, priority) {
         this.id = id;
-        this.claimantNino = claimantNino;
+        this.nino = nino;
         this.staffId = staffId;
         this.owningOfficeId = owningOfficeId;
         this.benefitId = benefitId;
         this.typeId = typeId;
         this.reasonId = reasonId;
-        this.timeAndDateRaised = this.setTimeAndDateRaised();
-        this.targetTime = this.calculateTargetTime();
+        this.timeAndDateRaised;
+        this.targetTime;
         this.callback = callback;
         this.priority = priority;
+        this.attachments = [];
+        this.notes = [];
+    }
+
+    addAttachment(attachment) {
+        this.attachments.push(attachment);
+    }
+
+    addNote(note) {
+        this.notes.push(note);
+    }
+
+    setTimeAndDateRaised() {
+        this.timeAndDateRaised = new Date();
     }
 
     // For now, this simply adds three hours if it is a callback type, or return null if not callback type.
     // Will need to be offset for service hours in real application if callback type.
-
-    setTimeAndDateRaised() {
-        this.timeAndDateRaised = new Date();
-
-    }
 
     calculateTargetTime() {
 
@@ -27,8 +36,7 @@ class Handover {
         if (this.callback === 1) {
             targetTime.setHours(targetTime.getHours() + 3);
         }
-        return targetTime;
-
+        this.targetTime = targetTime;
     }
 }
 
