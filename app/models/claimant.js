3d4bed8fa04cd826be4f601d6c932512fe0ae589
jane.gleadall <jane.gleadall@dwp.gsi.gov.uk>
2017-11-03 15:02:50 +0000

    Added input fields and views for welsh speaker field on claimant

--- app/models/claimant.js
+++ app/models/claimant.js
@@ -1,5 +1,5 @@
 class Claimant {
-    constructor(firstName, lastName, dob, nino, telNum, mobile, postcode) {
+    constructor(firstName, lastName, dob, nino, telNum, mobile, postcode, welshSpeaker) {
         this.nino = nino;
         this.firstName = firstName;
         this.lastName = lastName;
@@ -7,6 +7,7 @@ class Claimant {
         this.telNum = telNum;
         this.mobile = mobile;
         this.postcode = postcode;
+        this.welshSpeaker = welshSpeaker;
     }
 }
 
